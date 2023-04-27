b5e8fb8a192b7f5943f3bc1d4ec126506a84c8bf
iamudesharma <udesh2568@gmail.com>
2023-04-27 16:24:13 +0530

    signIn fixed

--- foodie_app_flutter/lib/page/sign_in_page.dart
+++ foodie_app_flutter/lib/page/sign_in_page.dart
@@ -1,11 +1,15 @@
+import 'dart:async';
+
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:foodie_app_flutter/page/home_page.dart';
 import 'package:serverpod_auth_firebase_flutter/serverpod_auth_firebase_flutter.dart';
+import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
 
 import '../main.dart';
+import 'package:serverpod_auth_client/module.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -15,27 +19,73 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
-  @override
-  void initState() {
-    signInWithFirebase(
-      context: context,
-      caller: client.modules.auth,
-      authProviders: [EmailAuthProvider(), PhoneAuthProvider()],
-    ).then((value) {
-      // If the user signed in, go to the home page
-      if (value != null) {
-        Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(
-              builder: (context) => const HomePage(),
-            ),
-            (route) => false);
-      }
-    });
-    super.initState();
-  }
+  var completer = Completer<UserInfo?>();
+
+  // @override
+  // void initState() {
+  //   signInWithFirebase(
+  //     context: context,
+  //     caller: client.modules.auth,
+  //     authProviders: [EmailAuthProvider(), PhoneAuthProvider()],
+  //   ).then((value) {
+  //     // If the user signed in, go to the home page
+  //     if (value != null) {
+
+  //     }
+  //   });
+  // super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return PlatformScaffold();
+    return PlatformScaffold(
+      body: SignInScreen(
+        providers: [EmailAuthProvider(), PhoneAuthProvider()],
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) async {
+            if (state.user == null) {
+              completer.complete(null);
+            } else {
+              var user = state.user!;
+
+              try {
+                var idToken = await user.getIdToken();
+                var serverResponse =
+                    await client.modules.auth.firebase.authenticate(idToken);
+
+                if (!serverResponse.success &&
+                    serverResponse.userInfo != null) {
+                  // Failed to sign in.
+                  completer.complete(null);
+                  return;
+                }
+
+                // Store the user info in the session manager.
+                var sessionManager = await SessionManager.instance;
+                await sessionManager.registerSignedInUser(
+                  serverResponse.userInfo!,
+                  serverResponse.keyId!,
+                  serverResponse.key!,
+                );
+
+                completer.complete(serverResponse.userInfo);
+
+                // ignore: use_build_context_synchronously
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ),
+                    (route) => false);
+
+                return;
+              } catch (e) {
+                completer.complete(null);
+                return;
+              }
+            }
+          })
+        ],
+      ),
+    );
   }
 }
