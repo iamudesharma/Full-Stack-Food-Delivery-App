5e2e00085749922af35de8dbfb335e0db48019ad
iamudesharma <udesh2568@gmail.com>
2023-04-27 23:23:54 +0530

    new method added

--- foodie_app_client/lib/src/protocol/client.dart
+++ foodie_app_client/lib/src/protocol/client.dart
@@ -7,46 +7,86 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:serverpod_client/serverpod_client.dart' as _i1;
-import 'package:serverpod_auth_client/module.dart' as _i2;
-import 'dart:io' as _i3;
-import 'protocol.dart' as _i4;
+import 'dart:async' as _i2;
+import 'package:foodie_app_client/src/protocol/users.dart' as _i3;
+import 'package:serverpod_auth_client/module.dart' as _i4;
+import 'dart:io' as _i5;
+import 'protocol.dart' as _i6;
+
+class _EndpointExample extends _i1.EndpointRef {
+  _EndpointExample(_i1.EndpointCaller caller) : super(caller);
+
+  @override
+  String get name => 'example';
+
+  _i2.Future<String> hello(String name) => caller.callServerEndpoint<String>(
+        'example',
+        'hello',
+        {'name': name},
+      );
+}
 
 class _EndpointUsers extends _i1.EndpointRef {
   _EndpointUsers(_i1.EndpointCaller caller) : super(caller);
 
   @override
   String get name => 'users';
+
+  _i2.Future<bool> checkUserExists() => caller.callServerEndpoint<bool>(
+        'users',
+        'checkUserExists',
+        {},
+      );
+
+  _i2.Future<void> createUser(_i3.User user) => caller.callServerEndpoint<void>(
+        'users',
+        'createUser',
+        {'user': user},
+      );
+
+  _i2.Future<bool> deleteUsers(_i3.User user) =>
+      caller.callServerEndpoint<bool>(
+        'users',
+        'deleteUsers',
+        {'user': user},
+      );
 }
 
 class _Modules {
   _Modules(Client client) {
-    auth = _i2.Caller(client);
+    auth = _i4.Caller(client);
   }
 
-  late final _i2.Caller auth;
+  late final _i4.Caller auth;
 }
 
 class Client extends _i1.ServerpodClient {
   Client(
     String host, {
-    _i3.SecurityContext? context,
+    _i5.SecurityContext? context,
     _i1.AuthenticationKeyManager? authenticationKeyManager,
   }) : super(
           host,
-          _i4.Protocol(),
+          _i6.Protocol(),
           context: context,
           authenticationKeyManager: authenticationKeyManager,
         ) {
+    example = _EndpointExample(this);
     users = _EndpointUsers(this);
     modules = _Modules(this);
   }
+
+  late final _EndpointExample example;
 
   late final _EndpointUsers users;
 
   late final _Modules modules;
 
   @override
-  Map<String, _i1.EndpointRef> get endpointRefLookup => {'users': users};
+  Map<String, _i1.EndpointRef> get endpointRefLookup => {
+        'example': example,
+        'users': users,
+      };
   @override
   Map<String, _i1.ModuleEndpointCaller> get moduleLookup =>
       {'auth': modules.auth};
