7e426876c3c967e75448398133b2ae2a8b38fa05
iamudesharma <udesh2568@gmail.com>
2023-04-27 16:13:10 +0530

    home _page added

---
+++ foodie_app_flutter/lib/page/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:foodie_app_flutter/main.dart';
+import 'package:foodie_app_flutter/page/sign_in_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    if (!sessionManager.isSignedIn) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => const SignInPage()),
+          (route) => false);
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformScaffold(
+        appBar: PlatformAppBar(trailingActions: [
+          PlatformIconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              await sessionManager.signOut();
+
+              await Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) => const SignInPage()),
+                  (route) => false);
+            },
+          )
+        ]),
+        body: const Center(
+          child: Text('Home Page'),
+        ));
+  }
+}
