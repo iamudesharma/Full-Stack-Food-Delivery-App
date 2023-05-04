7fc1cc890dc51da1e9266fdc57099bbcbe01a24f
iamudesharma <udesh2568@gmail.com>
2023-05-04 11:05:06 +0530

    AdvancedDrawer controller added

--- foodie_app_flutter/lib/page/home_page.dart
+++ foodie_app_flutter/lib/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:foodie_app_client/foodie_app_client.dart';
@@ -19,6 +20,8 @@ class HomePage extends StatefulWidget {
 ValueNotifier<User?> user = ValueNotifier(null);
 
 class _HomePageState extends State<HomePage> {
+  final _advancedDrawerController = AdvancedDrawerController();
+
   @override
   void initState() {
     if (mounted) {
@@ -53,93 +56,114 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return ValueListenableBuilder<User?>(
       valueListenable: user,
-      builder: (context, value, child) => PlatformScaffold(
-        bottomNavBar: PlatformNavBar(
-          material: (context, platform) => MaterialNavBarData(
-              type: BottomNavigationBarType.fixed,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              // selectedItemColor: Colors/,
-              unselectedItemColor: Colors.grey,
-              backgroundColor: Colors.transparent,
-              elevation: 0.0,
-              currentIndex: 0,
-              items: [
-                BottomNavigationBarItem(
-                  icon: Icon(PlatformIcons(context).home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(PlatformIcons(context).favoriteOutline),
-                  label: 'fav',
-                ),
-                const BottomNavigationBarItem(
-                  icon: Icon(Icons.restore_rounded),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(PlatformIcons(context).search),
-                  label: 'Search',
-                ),
-              ]),
-          backgroundColor: Colors.transparent,
-        ),
-        body: SafeArea(
-          child: CustomScrollView(slivers: [
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10,
-                ),
-                child: Row(children: const [
-                  Icon(Icons.menu_sharp),
-                  Spacer(),
-                  Icon(Icons.card_travel)
+      builder: (context, value, child) => AdvancedDrawer(
+        controller: _advancedDrawerController,
+        drawer: Container(),
+        child: PlatformScaffold(
+          bottomNavBar: PlatformNavBar(
+            material: (context, platform) => MaterialNavBarData(
+                type: BottomNavigationBarType.fixed,
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                // selectedItemColor: Colors/,
+                unselectedItemColor: Colors.grey,
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                currentIndex: 0,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(PlatformIcons(context).home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(PlatformIcons(context).favoriteOutline),
+                    label: 'fav',
+                  ),
+                  const BottomNavigationBarItem(
+                    icon: Icon(Icons.restore_rounded),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(PlatformIcons(context).search),
+                    label: 'Search',
+                  ),
                 ]),
-              ),
-            ),
-            SliverPadding(
-              padding: const EdgeInsets.all(15),
-              sliver: SliverToBoxAdapter(
-                child: DefaultTextStyle(
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+            backgroundColor: Colors.transparent,
+          ),
+          body: SafeArea(
+            child: CustomScrollView(slivers: [
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      PlatformText("Delicious").animate().slideX(),
-                      PlatformText("Food for you").animate().slideX(),
-                    ],
+                  child: Row(children: [
+                    PlatformIconButton(
+                        icon: ValueListenableBuilder<AdvancedDrawerValue>(
+                            valueListenable: _advancedDrawerController,
+                            builder: (context, value, child) {
+                              return AnimatedSwitcher(
+                                duration: const Duration(milliseconds: 250),
+                                child: value.visible
+                                    ? const Icon(Icons.remove_circle)
+                                        .animate()
+                                        .scaleY()
+                                    : Icon(PlatformIcons(context).accountCircle)
+                                        .animate()
+                                        .scaleY(),
+                              );
+                            }),
+                        onPressed: () {
+                          _advancedDrawerController.showDrawer();
+                        }),
+                    const Spacer(),
+                    const Icon(Icons.card_travel)
+                  ]),
+                ),
+              ),
+              SliverPadding(
+                padding: const EdgeInsets.all(15),
+                sliver: SliverToBoxAdapter(
+                  child: DefaultTextStyle(
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        PlatformText("Delicious").animate().slideX(),
+                        PlatformText("Food for you").animate().slideX(),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            SliverToBoxAdapter(
-              child: Container(
-                decoration: BoxDecoration(
-                  backgroundBlendMode: BlendMode.saturation,
-                  border: Border.all(
-                    color: Colors.grey.withOpacity(0.5),
+              SliverToBoxAdapter(
+                child: Container(
+                  decoration: BoxDecoration(
+                    backgroundBlendMode: BlendMode.saturation,
+                    border: Border.all(
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                    color: const Color(0xffEFEEEE),
                   ),
-                  borderRadius: BorderRadius.circular(30),
-                  color: const Color(0xffEFEEEE),
+                  height: 50,
+                  // width: 200,
+                  margin: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(mainAxisSize: MainAxisSize.min, children: [
+                    const SizedBox(width: 10),
+                    Icon(PlatformIcons(context).search),
+                    const SizedBox(width: 10),
+                    PlatformText("Search"),
+                  ]),
                 ),
-                height: 50,
-                // width: 200,
-                margin: EdgeInsets.symmetric(horizontal: 25),
-                child: Row(mainAxisSize: MainAxisSize.min, children: [
-                  const SizedBox(width: 10),
-                  Icon(PlatformIcons(context).search),
-                  const SizedBox(width: 10),
-                  PlatformText("Search"),
-                ]),
-              ),
-            )
-          ]),
+              )
+            ]),
+          ),
         ),
       ),
     );
