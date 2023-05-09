f5b85e4194a1cae4dfeb6d92dd1d52931d282e35
iamudesharma <udesh2568@gmail.com>
2023-05-09 16:37:42 +0530

    sign templated added

--- package_for_food_app/lib/src/client.dart
+++ package_for_food_app/lib/src/client.dart
@@ -12,8 +12,8 @@ import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart
 import 'package:serverpod_flutter/serverpod_flutter.dart';
 // import 'package:s';
 
-class ServerPodClient {
-  ServerPodClient._() {
+class ServerPodClientApp {
+  ServerPodClientApp._() {
     client = Client(
         'http://${Platform.isAndroid ? "10.0.2.2" : "localhost"}:8080/',
         authenticationKeyManager: FlutterAuthenticationKeyManager())
@@ -22,14 +22,13 @@ class ServerPodClient {
     sessionManager = sessionManager = SessionManager(
       caller: client.modules.auth,
     );
-    _sessionInit();
   }
 
   static late Client client;
 
   static late SessionManager sessionManager;
 
-  static _sessionInit() async {
-    await sessionManager.initialize();
+  static Future<bool> sessionInit() async {
+    return await sessionManager.initialize();
   }
 }
---
+++ package_for_food_app/lib/src/sign_in_templeted.dart
@@ -0,0 +1,122 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:async';
+
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+// import 'package:foodie_app_flutter/page/user_setup.dart';
+// import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:package_for_food_app/package_for_food_app.dart';
+import 'package:serverpod_auth_client/module.dart';
+import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
+
+import 'client.dart';
+
+// import 'package:foodie_app_flutter/page/home_page.dart';
+
+// import '../main.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({
+    Key? key,
+    this.onSignIn,
+    this.onSignFailed,
+  }) : super(key: key);
+
+  final Function()? onSignIn;
+  final Function()? onSignFailed;
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  var completer = Completer<UserInfo?>();
+
+  // @override
+  // void initState() {
+  //   signInWithFirebase(
+  //     context: context,
+  //     caller: client.modules.auth,
+  //     authProviders: [EmailAuthProvider(), PhoneAuthProvider()],
+  //   ).then((value) {
+  //     // If the user signed in, go to the home page
+  //     if (value != null) {
+
+  //     }
+  //   });
+  // super.initState();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformScaffold(
+      body: SignInScreen(
+        providers: [EmailAuthProvider(), PhoneAuthProvider()],
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) async {
+            if (state.user == null) {
+              completer.complete(null);
+            } else {
+              var user = state.user!;
+
+              try {
+                var idToken = await user.getIdToken();
+                var serverResponse =
+                    await ServerPodClientApp.client.
+                    
+                    modules.auth.firebase.authenticate(idToken);
+
+                if (!serverResponse.success &&
+                    serverResponse.userInfo != null) {
+                  // Failed to sign in.
+                  completer.complete(null);
+                  return;
+                }
+
+                // Store the user info in the session manager.
+                var sessionManager = await SessionManager.instance;
+                await sessionManager.registerSignedInUser(
+                  serverResponse.userInfo!,
+                  serverResponse.keyId!,
+                  serverResponse.key!,
+                );
+
+                completer.complete(serverResponse.userInfo);
+
+
+                widget.onSignIn;
+
+                // if (await await ServerPodClientApp
+                //     .client.modules.users.checkUserExists()) {
+                //   // ignore: use_build_context_synchronously
+                //   Navigator.of(context).pushAndRemoveUntil(
+                //       MaterialPageRoute(
+                //         builder: (context) => const UserSetupPage(),
+                //       ),
+                //       (route) => false);
+                // } else {
+                //   // ignore: use_build_context_synchronously
+                //   Navigator.of(context).pushAndRemoveUntil(
+                //       MaterialPageRoute(
+                //         builder: (context) => const HomePage(),
+                //       ),
+                //       (route) => false);
+                // }
+                // // // ignore: use_build_context_synchronously
+
+                return;
+              } catch (e) {
+                completer.complete(null);
+
+                widget.onSignFailed;
+                return;
+              }
+            }
+          })
+        ],
+      ),
+    );
+  }
+}
